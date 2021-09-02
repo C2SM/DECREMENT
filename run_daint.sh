cc9f0df9eb70c42a0f54fc77a2e5192c65530cbb
Matthieu Leclair <matthieu.leclair@env.ethz.ch>
2021-09-02 09:09:45 +0200

    FIX: syntax error with whitespace

--- run_daint.sh
+++ run_daint.sh
@@ -23,8 +23,8 @@ fi
 # Spack Executables
 # =================
 
-[[-z ${LM_INT2LM_SPEC} ]] || rsync -av $(spack location -i ${INT2LM_SPEC})/bin ./bin/$(basename ${EXE_INT2LM})
-[[-z ${LM_COSMO_SPEC} ]] || rsync -av $(spack location -i ${COSMO_SPEC})/bin/cosmo_${COSMO_TARGET} ./bin/$(basename ${EXE_COSMO})
+[[ -z ${LM_INT2LM_SPEC} ]] || rsync -av $(spack location -i ${INT2LM_SPEC})/bin ./bin/$(basename ${EXE_INT2LM})
+[[ -z ${LM_COSMO_SPEC} ]] || rsync -av $(spack location -i ${COSMO_SPEC})/bin/cosmo_${COSMO_TARGET} ./bin/$(basename ${EXE_COSMO})
 
 
 # Read simualtion config
