3f228bb56d1ff68b528dd2bf0450709259773684
Matthieu Leclair <matthieu.leclair@env.ethz.ch>
2023-03-02 09:49:01 +0100

    REF: modularize user settings further
    Implement default namelists in dedicated functions that are git
    tracked. If one wants to change a paramter that isn't specified by an
    environment variable, it is still possible to redefine and export the
    namelist function in a file that has higher priority (user_settings >
    config > default). This way the configuration is fully customizable
    without the need to modify the `run` scripts which are git tracked.

---
+++ Default_namelists/lm_f_defaults.sh
@@ -0,0 +1,359 @@
+#!/bin/bash
+
+# ---------------------------------------------------------------
+#                            INPUT_ORG
+# ---------------------------------------------------------------
+lm_f_INPUT_ORG(){
+    cat > INPUT_ORG << EONL
+ &LMGRID
+  startlat_tot = $LM_NL_STARTLAT_TOT_F
+  startlon_tot = $LM_NL_STARTLON_TOT_F
+  pollat = $LM_NL_POLLATLM_F
+  pollon = $LM_NL_POLLONLM_F
+  dlon = $LM_NL_DLONLM_F
+  dlat = $LM_NL_DLATLM_F
+  ie_tot = $LM_NL_IELM_F
+  je_tot = $LM_NL_JELM_F
+  ke_tot = $LM_NL_KELM_F
+ /END
+
+ &RUNCTL
+  dt = $LM_NL_DT_F
+  hstart = $LM_NL_HSTART
+  hstop = $LM_NL_HSTOP
+  ydate_ini = '${LM_YYYY_INI}${LM_MM_INI}${LM_DD_INI}${LM_ZZ_INI}0000'
+  hincmxt = 24.0
+  hincmxu = 24.0
+  itype_timing = 4
+  lreproduce = .TRUE.
+  luseobs =.FALSE.
+  lphys =.TRUE.
+  ldiagnos = .FALSE.
+  luse_rttov = .FALSE.
+  ldfi = .FALSE.
+  nprocx = $NQS_NXLM_F
+  nprocy = $NQS_NYLM_F
+  nprocio = 0
+  num_asynio_comm = $NQS_NIOLM_F
+  num_iope_percomm = $LM_NL_NUM_IOPE_PERCOMM_F
+  asynio_block_size = $LM_NL_ASYNIO_BLOCK_SIZE_F
+  nboundlines = 3
+  ldump_ascii = .FALSE.
+  itype_calendar = 0
+  idbg_level = 4
+ /END
+
+ &TUNING
+  tkhmin = $LM_NL_TKHMIN_F
+  tkmmin = $LM_NL_TKMMIN_F
+  mu_rain = 0.5
+  v0snow = $LM_NL_V0SNOW_F
+  rlam_mom = 0.0
+  rlam_heat = $LM_NL_RLAM_HEAT_F
+  rat_sea = 20.0
+  rat_lam = 1.0
+  rat_can = 1.0
+  c_lnd = 2.0
+  c_sea = 1.5
+  c_soil = 1.0
+  pat_len = 500.0
+  z0m_dia = 0.2
+  crsmin = 150.0
+  clc_diag = 0.5
+  q_crit = $LM_NL_Q_CRIT_F
+  qc0 = $LM_NL_QC0_F
+  qi0 = 5E-6
+  uc1 = 0.0626
+  tur_len = $LM_NL_TUR_LEN_F
+ /END
+EONL
+}
+export -f lm_f_INPUT_ORG
+
+# ---------------------------------------------------------------
+#                            INPUT_DYN
+# ---------------------------------------------------------------
+lm_f_INPUT_DYN(){
+    cat > INPUT_DYN << EONL
+ &DYNCTL
+  lcpp_dycore = ${LM_NL_LCPP_DYCORE}
+  lspubc = .TRUE.
+  itype_spubc = 3
+  rdheight = $LM_NL_RDHEIGHT_F
+  nrdtau = $LM_NL_NRDTAU_F
+  rlwidth = $LM_NL_RLWIDTH_F
+  ldyn_bbc = .FALSE.
+  itype_bbc_w = 114
+  betasw = 0.4
+  xkd = 0.1
+  epsass = 0.15
+  lcond = .TRUE.
+  l_diff_smag = $LM_NL_L_DIFF_SMAG_F
+  l_diff_cold_pools = $LM_NL_LDIFF_COLD_POOLS_F
+  lhordiff = .TRUE.
+  itype_hdiff = 2
+  hd_corr_u_bd = $LM_NL_HD_CORR_U_BD_F
+  hd_corr_t_bd = $LM_NL_HD_CORR_T_BD_F
+  hd_corr_trcr_bd = $LM_NL_HD_CORR_TRCR_BD_F
+  hd_corr_p_bd = $LM_NL_HD_CORR_P_BD_F
+  hd_corr_u_in = $LM_NL_HD_CORR_U_IN_F
+  hd_corr_t_in = $LM_NL_HD_CORR_T_IN_F
+  hd_corr_trcr_in = $LM_NL_HD_CORR_TRCR_IN_F
+  hd_corr_p_in = $LM_NL_HD_CORR_P_IN_F
+  hd_dhmax = 250.
+  l2tls = .TRUE.
+  irunge_kutta = 1
+  irk_order = 3
+  iadv_order = 5
+  itheta_adv = 0
+  ltadv_limiter = .FALSE.
+  itype_fast_waves = 2
+  y_scalar_advect = 'BOTT2_STRANG'
+  y_vert_adv_dyn = 'impl2'
+  ieva_order = 3
+  itype_outflow_qrsg = 2
+  itype_lbc_qrsg = 1
+ /END
+EONL
+}
+export -f lm_f_INPUT_DYN
+
+# ---------------------------------------------------------------
+#                            INPUT_PHY
+# ---------------------------------------------------------------
+lm_f_INPUT_PHY(){
+    cat > INPUT_PHY << EONL
+ &PHYCTL
+  lseaice = .FALSE.
+  llake = .FALSE.
+  lgsp = .TRUE.
+  lgsp_first = .TRUE.
+  itype_gscp = $LM_NL_ITYPE_GSCP_F
+  ldiniprec = .FALSE.
+  lrad = .TRUE.
+  hincrad = $LM_NL_HINCRAD_F
+  lradtopo = .FALSE.
+  ico2_rad = $LM_NL_ICO2_RAD_F
+  lforest = .TRUE.
+  ltur = .TRUE.
+  loldtur = .TRUE.
+  ninctura = 1
+  itype_vdif = -1
+  lexpcor = .FALSE.
+  ltmpcor = .FALSE.
+  lprfcor = .FALSE.
+  lnonloc = .FALSE.
+  lcpfluc = .FALSE.
+  itype_turb = 3
+  imode_turb = 1
+  itype_tran = 2
+  imode_tran = 1
+  limpltkediff = .TRUE.
+  itype_wcld = 2
+  icldm_rad = 4
+  icldm_turb = 2
+  icldm_tran = 0
+  itype_synd = 2
+  lsoil = .TRUE.
+  itype_hydmod = 1
+  lsoil_init_fill = 1
+  lmelt = .TRUE.
+  lmelt_var = .TRUE.
+  itype_trvg = 2
+  ke_soil = 9
+  czml_soil = 0.005, 0.025, 0.07, 0.16, 0.34, 0.70, 1.47, 2.86, 5.74, 11.50
+  czbot_w_so = 8.0
+  itype_root = 2
+  itype_heatcond = 2
+  itype_evsl = 4
+  lconv = $LM_NL_LCONV_F
+  itype_conv = 3
+  nincconv = 5
+  lcape = .FALSE.
+  lconf_avg = .TRUE.
+  lsso = $LM_NL_LSSO_F
+  ltkesso = .False.
+  ltkeshs = .False.
+  itype_albedo = $LM_NL_ITYPE_ALBEDO_F
+  itype_aerosol = $LM_NL_ITYPE_AEROSOL_F
+ /END
+EONL
+}
+export -f lm_f_INPUT_PHY
+
+# ---------------------------------------------------------------
+#                           INPUT_IO
+# ---------------------------------------------------------------
+lm_f_INPUT_IO(){
+    cat > INPUT_IO << EONL
+ &IOCTL
+  ldwd_grib_use = .FALSE.
+  yform_read = 'ncdf'
+  l_ke_in_gds = .TRUE.
+  lasync_io = $LM_NL_LASYNC_IO_F
+  lprefetch_io = ${LM_NL_LPREFETCH_F:-.FALSE.}
+  ymode_read = 'r  '
+  ymode_write = 'w  '
+  nincwait = 10
+  nmaxwait = 200
+  nhour_restart = $LM_NL_HSTOP, $LM_NL_HSTOP, 1
+  ngribout = 3
+  itype_gather = 2
+  ydir_restart_in = 'output/restart'
+  ydir_restart_out = 'output/restart'
+  ytunit_restart = 'd'
+  lbdclim = .TRUE.
+  yncglob_title = "COSMO driven by 4 km COSMO"
+  yncglob_source = "COSMO_5.08"
+ /END
+
+ &DATABASE
+ /END
+
+ &GRIBIN
+  lbdana = .FALSE.
+  ydirini = 'input'
+  lchkini = .TRUE.
+  hincbound = $LM_NL_HINCBOUND_F
+  ydirbd = 'input'
+  lchkbd = .FALSE.
+  lana_qi = $LM_NL_LANA_QI_F
+  llb_qi = $LM_NL_LANA_QI_F
+  lana_qg = .FALSE.
+  llb_qg = .FALSE.
+  lana_qr_qs = .FALSE.
+  llb_qr_qs = .FALSE.
+  lana_rho_snow = $LM_NL_LAN_RHO_SNOW_F
+  lan_lai = .TRUE.
+  lan_rootdp = .TRUE.
+  lan_vio3 = .TRUE.
+  lan_plcov = .TRUE.
+  lan_t_cl = .TRUE.
+  lan_w_cl = .TRUE.
+  lan_hmo3 = .TRUE.
+  lan_t_so0 = .TRUE.
+  lan_t_snow = .TRUE.
+  lan_w_snow = .TRUE.
+  lan_w_i = .TRUE.
+  lan_rho_snow = .TRUE.
+  lan_t_s = .FALSE.
+  ytunitbd = 'd'
+ /END
+
+ &GRIBOUT
+  yform_write = 'nc-4'
+  hcomb = 0.0, ${LM_NL_HSTOP}, 1
+  yvarml = 'U_10M', 'V_10M', 'T_2M', 'RELHUM_2M', 'PS', 'QV_2M',
+           'ALHFL_S', 'ASHFL_S', 'AUMFL_S', 'AVMFL_S',
+           'CAPE_ML', 'CIN_ML', 'CLCH', 'CLCM', 'CLCL', 'TOT_PREC',
+           'TQV', 'TQC', 'TQI','TQR','TQG','TQS',
+           'ASOB_T', 'ASOD_T',  'ATHB_T', 'ASOBC_T', 'ATHBC_T',
+           'ASOB_S', 'ASWDIFD_S','ASWDIR_S', 'ASWDIFU_S', 'ATHB_S', 'ATHD_S',
+           'ASOBC_S', 'ATHBC_S'
+  yvarpl = ' '
+  yvarzl = ' '
+  ireset_sums = 2
+  lcheck = .TRUE.
+  luvmasspoint = .FALSE.
+  lwrite_const = .TRUE.
+  ydir = 'output/1h_2D'
+  l_z_filter = .FALSE.
+  l_p_filter = .FALSE.
+  l_fi_pmsl_smooth = .FALSE.
+  ytunit = 'd'
+ /END
+
+ &GRIBOUT
+  yform_write = 'nc-4'
+  hcomb = 0.0, ${LM_NL_HSTOP}, 3
+  yvarml = ' '
+  yvarpl = ' '
+  yvarzl = 'U', 'V', 'W', 'T', 'P', 'QV', 'QC', 'QI'
+  zlev = 100, 200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000,
+         2300, 2600, 2900, 3200, 3500, 4000, 4500,
+         5000, 6000, 7000, 8000, 9000, 10000
+  lcheck = .FALSE.
+  luvmasspoint = .FALSE.
+  lwrite_const = .FALSE.
+  ydir = 'output/3h_3D_zlev'
+  l_z_filter = .FALSE.
+  l_p_filter = .FALSE.
+  l_fi_pmsl_smooth = .FALSE.
+  ytunit = 'd'
+ /END
+
+ &GRIBOUT
+  yform_write = 'nc-4'
+  hcomb = 0.0, ${LM_NL_HSTOP}, 24
+  yvarml = 'VMAX_10M', 'W_SO', 'TMIN_2M', 'TMAX_2M', 'RUNOFF_S', 'RUNOFF_G'
+  yvarpl = ' '
+  yvarzl = ' '
+  ireset_winds = 2
+  ireset_temps = 2
+  lcheck = .FALSE.
+  luvmasspoint = .FALSE.
+  lwrite_const = .FALSE.
+  ydir = 'output/24h'
+  l_z_filter = .FALSE.
+  l_p_filter = .FALSE.
+  l_fi_pmsl_smooth = .FALSE.
+  ytunit = 'd'
+ /END
+EONL
+}
+export -f lm_f_INPUT_IO
+
+# ---------------------------------------------------------------
+#                            INPUT_DIA
+# ---------------------------------------------------------------
+lm_f_INPUT_DIA(){
+    cat > INPUT_DIA << EONL
+ &DIACTL
+  n0meanval = 0
+  nincmeanval = 1
+ /END
+EONL
+}
+export -f lm_f_INPUT_DIA
+
+# ---------------------------------------------------------------
+#                            INPUT_INI
+# ---------------------------------------------------------------
+lm_f_INPUT_INI(){
+    cat > INPUT_INI << EONL
+ &INICTL
+  ndfi = 2
+  tspan = 1840.0
+  taus = 1840.0
+  dtbak = 20.0
+  dtfwd = 20.0
+ /END
+EONL
+}
+export -f lm_f_INPUT_INI
+
+# ---------------------------------------------------------------
+#                            INPUT_SAT
+# ---------------------------------------------------------------
+lm_f_INPUT_SAT(){
+    cat > INPUT_SAT << EONL
+ &SATCTL
+  num_sensors = 1
+  sat_input_01 = 'MSG', 1, 'SEVIRI', 8, .TRUE., .TRUE., .TRUE., .TRUE.
+  nchan_input_01 = 1, 2, 3, 4, 5, 6, 7, 8
+  lcon_clw = .FALSE.
+ /END
+EONL
+}
+export -f lm_f_INPUT_SAT
+
+# ---------------------------------------------------------------
+#                            INPUT_ASS
+# ---------------------------------------------------------------
+lm_f_INPUT_ASS(){
+    cat > INPUT_ASS << EONL
+ &NUDGING
+ /END
+EONL
+}
+export -f lm_f_INPUT_ASS
--- run_daint.sh
+++ run_daint.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-# User defined parameters
-# =======================
+
+# Load default namelist definitions
+# =================================
+
+source Default_namelists/ifs2lm_defaults.sh
+source Default_namelists/lm_c_defaults.sh 
+source Default_namelists/lm2lm_defaults.sh 
+source Default_namelists/lm_f_defaults.sh
+
+
+# Load user defined parameters
+# ============================
+# Load once here, just to have access to COSMO_TARGET
+
 if [ ! -f user_settings ]; then 
-  echo "user_settings doesn't exist yet. Copying default file user_settings_example"
-  cp user_settings_example user_settings
+    echo "user_settings doesn't exist yet. Copying default file user_settings_example"
+    cp user_settings_example user_settings
 fi
 
 if [ ! -f config ]; then
-  echo "Abort. No config specifed. Copy one from simulation_configs, e.g., cp simulation_configs/SIMULATION_EU_CORDEX_50km config"
-  exit 1
+    echo "Abort. No config specifed. Copy one from simulation_configs, e.g., cp simulation_configs/SIMULATION_EU_CORDEX_50km config"
+    exit 1
 fi
 
 source user_settings
+
 
 # Daint specific settings
 # =======================
@@ -25,77 +38,112 @@ export GPUS_PER_NODE=1
 export LM_NTASKS_PER_NODE_INT2LM=12
 
 
-# Read simualtion config
+# Load simualtion config
 # ======================
 
 source config
 
-# Source user settings again so that they take precedence over the default simulation config
+
+# Reload user settings
+# ====================
+
+# Source user settings again so that they take precedence over anything else
+# user_settings > config > Default_namelists/*
 source user_settings
 
+# Compute requested node numbers
+# ==============================
+nodes(){
+    echo $(( ($1 * $2 + $3 + $4 - 1) / $4 ))
+}
+export LM_COSMO_NODES_C=$(nodes ${NQS_NXLM_C} ${NQS_NYLM_C} ${NQS_NIOLM_C} ${LM_NTASKS_PER_NODE_COSMO})
+export LM_COSMO_NODES_F=$(nodes ${NQS_NXLM_F} ${NQS_NYLM_F} ${NQS_NIOLM_F} ${LM_NTASKS_PER_NODE_COSMO})
+export LM_IFS2LM_NODES=$(nodes ${NQS_NXIFS2LM} ${NQS_NYIFS2LM} ${NQS_NIOIFS2LM} ${LM_NTASKS_PER_NODE_INT2LM})
+export LM_LM2LM_NODES=$(nodes ${NQS_NXLM2LM} ${NQS_NYLM2LM} ${NQS_NIOLM2LM} ${LM_NTASKS_PER_NODE_INT2LM})
 
-# Set Start and end
-# =================
 
-# Global init date components
+# Handle dates
+# ============
+# Convert dates to formats capable of handling addition of arbitrary time intervals
+export LM_INI_DATE=$(date -d "${LM_INI_DATE}" +%c)
+export LM_FIN_DATE=$(date -d "${LM_INI_DATE}" +%c)
+
+# Startdate of current step
+# If set in 6_chain/run, keep the value
+# otherwise if LM_START_DATE is set in user_settings, use that date
+# else start from absolute initial date LM_INI_DATE
+if [[ -z ${LM_BEGIN_DATE} ]]; then
+    if [[ -n ${LM_START_DATE} ]]; then
+        export LM_BEGIN_DATE=$(date -d "${LM_START_DATE}" +%c)
+    else
+        export LM_BEGIN_DATE=${LM_INI_DATE}
+    fi
+fi
+
+# Enddate of current step
+if [[ -z "${LM_CHAIN_INTERVAL}" ]]; then
+    export LM_END_DATE=${LM_FIN_DATE}
+else
+    END_DATE=$(date -d "${LM_BEGIN_DATE}+${LM_CHAIN_INTERVAL}" +%c)
+    if (( $(date -d "$END_DATE" +%s) > $(date -d "$LM_FIN_DATE" +%s) )); then
+        export LM_END_DATE=${LM_FIN_DATE}
+    else
+        export LM_END_DATE=${END_DATE}
+    fi
+fi
+
+# Store dates components
 export LM_YYYY_INI=$(date -d "${LM_INI_DATE}" +%Y)
 export LM_MM_INI=$(date -d "${LM_INI_DATE}" +%m)
 export LM_DD_INI=$(date -d "${LM_INI_DATE}" +%d)
 export LM_ZZ_INI=$(date -d "${LM_INI_DATE}" +%H)
 
-# Startdate of current step
-# If set in 6_chain/run, keep the value otherwise initialize with absolute start date
-export LM_BEGIN_DATE=${LM_BEGIN_DATE:-${LM_INI_DATE}}
 export LM_YYYY_BEGIN=$(date -d "${LM_BEGIN_DATE}" +%Y)
 export LM_MM_BEGIN=$(date -d "${LM_BEGIN_DATE}" +%m)
 export LM_DD_BEGIN=$(date -d "${LM_BEGIN_DATE}" +%d)   # - ML - unused
 export LM_ZZ_BEGIN=$(date -d "${LM_BEGIN_DATE}" +%H)   # - ML - unused
 
-# Enddate of current step
-if [[ -z "${LM_CHAIN_INTERVAL}" ]]; then
-  export LM_END_DATE=${LM_FIN_DATE}
-else
-  END_DATE=$(date -d "${LM_BEGIN_DATE}+${LM_CHAIN_INTERVAL}" +%Y-%m-%dT%H:00)
-  if (( $(date -d "$END_DATE" +%s) > $(date -d "$LM_FIN_DATE" +%s) )); then
-    export LM_END_DATE=${LM_FIN_DATE}
-  else
-    export LM_END_DATE=${END_DATE}
-  fi
-fi
 export LM_YYYY_END=$(date -d "${LM_END_DATE}" +%Y)
 export LM_MM_END=$(date -d "${LM_END_DATE}" +%m)
 export LM_DD_END=$(date -d "${LM_END_DATE}" +%d)
 export LM_ZZ_END=$(date -d "${LM_END_DATE}" +%H)
 
-#Compute HSTART and HEND of the simulation
+# Compute HSTART, HSTOP and corresponding NSTART, NSTOP
 diff_hours() {
-  d1=$(date -d "$1" +%s)
-  d2=$(date -d "$2" +%s)
-  echo $(( ($d2 - $d1) / 3600 ))
+    d1=$(date -d "$1" +%s)
+    d2=$(date -d "$2" +%s)
+    echo $(( ($d2 - $d1) / 3600 ))
 }
 export LM_NL_HSTART=$(diff_hours ${LM_INI_DATE} ${LM_BEGIN_DATE})
 export LM_NL_HSTOP=$(diff_hours ${LM_INI_DATE} ${LM_END_DATE})
+
+export LM_NL_NSTART_C=$((LM_NL_HSTART*3600/LM_NL_DT_C))
+export LM_NL_NSTOP_C=$((LM_NL_HSTOP*3600/LM_NL_DT_C))
+
+export LM_NL_NSTART_F=$((LM_NL_HSTART*3600/LM_NL_DT_F))
+export LM_NL_NSTOP_F=$((LM_NL_HSTOP*3600/LM_NL_DT_F))
+
 
 # Submit jobs
 # ===========
 
 for part in ${SB_PARTS} ; do
-  short=$(echo "${part}" | sed 's/^[0-9]*_//g')
-  echo "launching ${short}"
-  mkdir -p output/${short}
+    short=$(echo "${part}" | sed 's/^[0-9]*_//g')
+    echo "launching ${short}"
+    mkdir -p output/${short}
 
-  cd ${part}
-  
-  # Ensemble execution
-  if [ "${part}" = "2_lm_c" ] && [ ! -z $LM_NL_ENS_NUMBER_C ]; then
-    echo "running ${short} in ensemble mode with ${LM_NL_ENS_NUMBER_C} realizations"
-    jobid=$(./run_ensemble ${jobid})
+    cd ${part}
+    
+    # Ensemble execution
+    if [ "${part}" = "2_lm_c" ] && [ ! -z $LM_NL_ENS_NUMBER_C ]; then
+        echo "running ${short} in ensemble mode with ${LM_NL_ENS_NUMBER_C} realizations"
+        jobid=$(./run_ensemble ${jobid})
+        cd - 1>/dev/null 2>/dev/null
+        continue
+    fi
+
+    # Normal execution
+    jobid=$(./run ${jobid})
+    
     cd - 1>/dev/null 2>/dev/null
-    continue
-  fi
-
-  # Normal execution
-  jobid=$(./run ${jobid})
-  
-  cd - 1>/dev/null 2>/dev/null
 done
