34ae26b638c1d85906bfff3645ee66fa09f0675c
Matthieu Leclair <matthieu.leclair@env.ethz.ch>
2023-03-16 09:09:49 +0100

    fix(distclean): minor leftover

--- distclean.sh
+++ distclean.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # clean jobs
-jobid=""
 for part in [0-9]*_* ; do
   echo "cleaning ${part}"
   cd ${part}
   ./clean
-  \rm core *.out INPUT INPUT_* job *.swp 2>/dev/null
+  \rm core *.out INPUT INPUT_* *.swp 2>/dev/null
   cd - 1>/dev/null 2>/dev/null
 done
 
