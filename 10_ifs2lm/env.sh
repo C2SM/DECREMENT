44d95b96a10976ff4ce37d895bec542855f672f0
Matthieu <matthieu.leclair@env.ethz.ch>
2023-06-15 12:12:21 +0200

    fix(int2lm): missing definition of node numbers in env.sh
    for both 10_ifs2lm and 30_lm2lm

--- 10_ifs2lm/env.sh
+++ 10_ifs2lm/env.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Nodes
+# =====
+export NQS_NODES_IFS2LM=$(compute_cosmo_nodes ${NQS_NXIFS2LM} ${NQS_NYIFS2LM} ${NQS_NIOIFS2LM} ${CORES_PER_NODE})
+
 # Spack
 # =====
 [[ -e spack_env_int2lm.sh ]] && source spack_env_int2lm.sh
--- 30_lm2lm/env.sh
+++ 30_lm2lm/env.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Nodes
+# =====
+export NQS_NODES_LM2LM=$(compute_cosmo_nodes ${NQS_NXLM2LM} ${NQS_NYLM2LM} ${NQS_NIOLM2LM} ${CORES_PER_NODE})
+
 # Spack
 # =====
 [[ -e spack_env_int2lm.sh ]] && source spack_env_int2lm.sh
