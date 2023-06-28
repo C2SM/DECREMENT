2586d71eb6a0c1d9d2257f14baeb2aa9ed3f3a3a
Matthieu <matthieu.leclair@env.ethz.ch>
2023-06-28 15:10:52 +0200

    fix(cclm2): adapt number of CESM tasks when COSMO runs on gpu

--- 20_cclm2_c/env.sh
+++ 20_cclm2_c/env.sh
@@ -24,6 +24,7 @@ export LM_NL_NSTOP_C=$((LM_NL_HSTOP*3600/LM_NL_DT_C))
 # =======================
 if [[ $COSMO_TARGET == "gpu" ]]; then
     export NQS_NODES_CCLM2_C=$(compute_cosmo_nodes ${NQS_NXLM_C} ${NQS_NYLM_C} ${NQS_NIOLM_C} 1)
+    export CLM_C_NTASKS=$(( NQS_NODES_CCLM2_C * (CORES_PER_NODE - 1) ))
 else
     ((ADD_TASKS = NQS_NIOLM_C + CLM_C_NTASKS))
     export NQS_NODES_CCLM2_C=$(compute_cosmo_nodes ${NQS_NXLM_C} ${NQS_NYLM_C} ${ADD_TASKS} ${CORES_PER_NODE})
